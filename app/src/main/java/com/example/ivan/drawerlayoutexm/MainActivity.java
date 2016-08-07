426c6e2e56a35a0fed65e9db7c0212662dfe3bb5
Ivan <i.lauhin@gmail.com>
2016-08-07 16:41:49 +0300

    first commit.

---
+++ app/src/main/java/com/example/ivan/drawerlayoutexm/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.ivan.drawerlayoutexm;
+
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fabBtn;
+    private CoordinatorLayout rootLayout;
+    private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private DrawerLayout drawerLayout;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initInstance();
+    }
+
+    private void initInstance() {
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbarLayout);
+        collapsingToolbarLayout.setTitle("Design Library");
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+
+        rootLayout = (CoordinatorLayout) findViewById(R.id.root_layout);
+        fabBtn = (FloatingActionButton) findViewById(R.id.fab);
+
+        fabBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(rootLayout,"Hello, I'm Snackbar!", Snackbar.LENGTH_SHORT)
+                        .setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+
+                            }
+                        })
+                        .show();
+
+            }
+        });
+
+        navigation = (NavigationView) findViewById(R.id.navigation);
+        navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+
+                int id = item.getItemId();
+
+                switch (id) {
+                    case R.id.navItem1:
+                        break;
+                    case R.id.navItem2:
+                        break;
+                    case R.id.navItem3:
+                        break;
+                }
+                return false;
+            }
+        });
+    }
+}
